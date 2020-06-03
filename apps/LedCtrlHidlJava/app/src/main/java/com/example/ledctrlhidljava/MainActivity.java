426009fa5caa60691a3ada1d2aac0f19609f7799
lucifer <xiangdong@trustkernel.com>
2020-06-03 23:41:45 +0800

    fixup compile LedCtrlHidlJava error:class file for android.os.IHwInterface not found

--- apps/LedCtrlHidlJava/app/src/main/java/com/example/ledctrlhidljava/MainActivity.java
+++ apps/LedCtrlHidlJava/app/src/main/java/com/example/ledctrlhidljava/MainActivity.java
@@ -6,13 +6,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.RemoteException;
-import android.os.HwBinder;
 import my_demo.hardware.led.V1_0.ILed;
 
 public class MainActivity extends Activity {
     private final String TAG = "LedCtrlHidlJava";
     private Button btn_led = null;
-    private ILed led_server = null;
+    private ILed led_server;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +34,23 @@ public class MainActivity extends Activity {
     public void onClickLed(View v) {
         int ret;
 
-        if (btn_led.getText().equals("开灯") ) {
-            Log.d(TAG, "turn on led");
-            btn_led.setText("关灯");
-            if ( led_server.control(0) < 0 ) {
-                Log.e(TAG, "led_server.control(0) failed");
-            }
+        try {
+            if (btn_led.getText().equals("开灯") ) {
+                Log.d(TAG, "turn on led");
+                btn_led.setText("关灯");
+                if ( led_server.control(1) < 0 ) {
+                    Log.e(TAG, "led_server.control(0) failed");
+                }
 
-        } else {
-            Log.d(TAG, "turn off led");
-            btn_led.setText("开灯");
-            if ( led_server.control(1) < 0 ) {
-                Log.e(TAG, "led_server.control(1) failed");
+            } else {
+                Log.d(TAG, "turn off led");
+                btn_led.setText("开灯");
+                if ( led_server.control(0) < 0 ) {
+                    Log.e(TAG, "led_server.control(1) failed");
+                }
             }
+        } catch (RemoteException e) {
+            Log.e(TAG, "Exception in led_server.control() " + e);
         }
     }
 
